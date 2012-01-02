dfb5f2198d660477e0e956eaeac45bdc86f1e362
Chris Tompkinson <tompko@gmail.com>
2012-01-02 07:13:56 +0000

    GCD functions added to common file Implementation of the affine shift cipher

--- python/praxis.py
+++ python/praxis.py
@@ -1,3 +1,4 @@
+#Columnar transposition
 def make_key(text_len, keyword):
     key_len = len(keyword)
     columns = [range(text_len)[i::key_len] for i in range(key_len)]
@@ -21,3 +22,15 @@ def untranspose(cipher, keyword):
 
     return "".join(message)
 
+#Math utilities
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+def extended_gcd(a, b):
+    if b == 0:
+        return (1, 0)
+    q, r = a / b, a % b
+    s, t = extended_gcd(b, r)
+    return (t, s - q * t)
