6d385128e0d03649e005a95cd5ece2084c83f740
Chris Tompkinson <tompko@gmail.com>
2012-01-06 08:18:33 +0000

    Print the bottles of beer on the wall song

---
+++ python/ninety-nine-bottles-of-beer.py
@@ -0,0 +1,12 @@
+def bottle():
+    for i in range(99, 1, -1):
+        print i, "bottles of beer on the wall"
+        print i, "bottles of beer"
+        print "Take one down and pass it around"
+        print i-1, "bottles of beer on the wall"
+        print ""
+
+    print "Only one bottle of beer on the wall."
+    print "Only one bottle of beer."
+    print "Take it down and pass it around."
+    print "Now there's no more beer on the wall."
