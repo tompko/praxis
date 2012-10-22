b7c163dc30d76a6b3cb42d33da8e76df2455dd88
Chris Tompkinson <tompko@gmail.com>
2012-10-22 14:23:41 +0100

    Fix pylint errors and warnings

--- python/rpn_calculator.py
+++ python/rpn_calculator.py
@@ -1,3 +1,5 @@
+"""RPN Calculator: Evaluate numeric expressions at the command line"""
+
 import sys
 import operator
 
@@ -7,27 +9,27 @@ OPERATORS = {"+": operator.add,
              "/": operator.div}
 
 def rpn(expr, stack=None):
-	"""Evaluate an expression in reverse polish notation"""
-	if stack == None:
-		stack = []
+    """Evaluate an expression in reverse polish notation"""
+    if stack == None:
+        stack = []
 
-	expr = expr.split()
+    expr = expr.split()
 
-	for e in expr:
-		if e in OPERATORS:
-			b, a = stack.pop(), stack.pop()
-			stack.append(OPERATORS[e](a, b))
-		else:
-			stack.append(float(e))
+    for e in expr:
+        if e in OPERATORS:
+            b, a = stack.pop(), stack.pop()
+            stack.append(OPERATORS[e](a, b))
+        else:
+            stack.append(float(e))
 
-	return stack
+    return stack
 
 if __name__ == "__main__":
-	"""Read expressions from standard input and print the top of the stack
-	to standard output when a newline is encountered"""
-	stack = []
-	line = sys.stdin.readline()
-	while line.strip():
-		stack = rpn(line.strip(), stack)
-		print stack[-1]
-		line = sys.stdin.readline()
+    #Read expressions from standard input and print the top of the stack
+    #to standard output when a newline is encountered
+    STACK = []
+    LINE = sys.stdin.readline()
+    while LINE.strip():
+        STACK = rpn(LINE.strip(), STACK)
+        print STACK[-1]
+        LINE = sys.stdin.readline()
--- python/sieve_of_eratosthenes.py
+++ python/sieve_of_eratosthenes.py
@@ -1,40 +1,47 @@
+"""Sieve of Eratosthenes: A Python implementation of an ancient algorithm"""
+
 import unittest
 
 def sieve(n):
-	"""Return a list of all primes less than n"""
-	#All even numbers except 2 are not primes
-	primes = [False, False, True] + [True, False] * (n / 2)
+    """Return a list of all primes less than n"""
+    #All even numbers except 2 are not primes
+    primes = [False, False, True] + [True, False] * (n / 2)
 
-	#Start with 3
-	p = 3
+    #Start with 3
+    p = 3
 
-	while p*p <= n:
-		if primes[p]:
-			#p is prime, cross off all multiples of p, starting at the square 
-			#of p since all smaller multiples have already been crossed off
-			d = p*p
-			while d <= n:
-				primes[d] = False
-				d += p
-		p += 2
+    while p*p <= n:
+        if primes[p]:
+            #p is prime, cross off all multiples of p, starting at the square 
+            #of p since all smaller multiples have already been crossed off
+            d = p*p
+            while d <= n:
+                primes[d] = False
+                d += p
+        p += 2
 
-	#Build a list of the primes we've found
-	return [i for i in range(n) if primes[i]]
+    #Build a list of the primes we've found
+    return [i for i in range(n) if primes[i]]
 
 class TestSieve(unittest.TestCase):
-	def test_primes_under_10(self):
-		"""Test the primes under 10 and 100"""
-		self.assertEqual(sieve(10), [2,3,5,7])
-		self.assertEqual(sieve(100), [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31,
-		37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97])
+    """Test the sieve of eratosthenes
 
-	def test_primes_under_1000000(self):
-		"""Check the lengths of the list of primes up to 1000000"""
-		self.assertEqual(len(sieve(100)), 25)
-		self.assertEqual(len(sieve(1000)), 168)
-		self.assertEqual(len(sieve(10000)), 1229)
-		self.assertEqual(len(sieve(100000)), 9592)
-		self.assertEqual(len(sieve(1000000)), 78498)
+    Perform two tests: one against lists of primes less than a number; one
+    against the number of primes less than a number.
+    """
+    def test_primes_under_10(self):
+        """Test the primes under 10 and 100"""
+        self.assertEqual(sieve(10), [2, 3, 5, 7])
+        self.assertEqual(sieve(100), [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31,
+        37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97])
+
+    def test_primes_under_1000000(self):
+        """Check the lengths of the list of primes up to 1000000"""
+        self.assertEqual(len(sieve(100)), 25)
+        self.assertEqual(len(sieve(1000)), 168)
+        self.assertEqual(len(sieve(10000)), 1229)
+        self.assertEqual(len(sieve(100000)), 9592)
+        self.assertEqual(len(sieve(1000000)), 78498)
 
 if __name__ == "__main__":
-	unittest.main()
+    unittest.main()
